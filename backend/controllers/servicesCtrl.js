46b42085612450998b6557be5ba0cbe92721ad64
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-08-30 13:49:47 +0530

    Implement view all service backend

---
+++ backend/controllers/servicesCtrl.js
@@ -0,0 +1,50 @@
+import Services from "../models/servicesModel.js";
+
+const servicesCtrl = {
+  createService: async (req, res) => {
+    try {
+      const {
+        title,
+        description,
+        category,
+        location,
+        fee,
+        needBuyerAddress,
+        needDate,
+        isCOD,
+        isOnlinePayment,
+        image,
+      } = req.body;
+
+      if (!image) return res.status(400).json({ msg: "No image uploaded" });
+      const newService = new Services({
+        title,
+        description,
+        category,
+        location,
+        fee,
+        needBuyerAddress,
+        needDate,
+        isCOD,
+        isOnlinePayment,
+        image,
+      });
+
+      await newService.save();
+      res.status(200).json({ msg: "Service created" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  getServices: async (req, res) => {
+    try {
+      const services = await Services.find();
+      res.status(200).json(services);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+export default servicesCtrl;
