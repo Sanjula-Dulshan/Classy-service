7c327968b8dd21c5fe66b2b3573acf8d1c69ed65
Dilsha-thathsarani <it20240042@my.sliit.lk>
2022-09-01 19:31:37 +0530

    Add style to UI

--- frontend/src/components/ViewService.jsx
+++ frontend/src/components/ViewService.jsx
@@ -4,14 +4,16 @@ export default function ViewService() {
   return (
     <div>
       <div className="mt-5 ">
-        <i
-          class="fas fa-arrow-circle-left"
-          style={{ fontSize: "22px", marginLeft: "15%" }}
-        >
-          Back
-        </i>
+        <a href="#">
+          <i
+            class="fas fa-arrow-circle-left"
+            style={{ fontSize: "22px", marginLeft: "15%", color: "#FEA82F" }}
+          >
+            Back
+          </i>
+        </a>
         <form
-          className="container mt-5"
+          className="container mt-5 pb-4 shadow-lg p-3 mb-5 bg-white rounded"
           style={{ backgroundColor: "#FBFDF3", marginLeft: "15%" }}
         >
           <div>
@@ -48,24 +50,36 @@ export default function ViewService() {
           </h5>
           <br></br>
           <br></br>
-          <div className="extra content">
+          <div className="extra content ">
             <div
-              className="card mb-3 container"
+              className="card mb-3 container pb-3 shadow-lg p-3 mb-5 bg-white rounded"
               style={{ width: "45%", height: "25%" }}
             >
               <div className="row g-0">
-                <div className="col-md-2">
+                <div className="col-md-2 mt-2">
                   <img
-                    src="..."
-                    className="img-fluid rounded-start"
-                    alt="..."
+                    src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
+                    className="rounded-circle shadow-4"
+                    style={{ height: "80%", width: "80%" }}
+                    alt="Avatar"
                   />
                 </div>
                 <div className="col-md-4">
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+                    <h5 className="card-title">
+                      <b>Card title</b>
+                    </h5>
                   </div>
-                  <div class="ui button">Buy</div>
+                  <div
+                    class="ui button"
+                    style={{
+                      backgroundColor: "#FEA82F",
+                      color: "black",
+                      marginLeft: "8%",
+                    }}
+                  >
+                    View Profile
+                  </div>
                 </div>
               </div>
             </div>
