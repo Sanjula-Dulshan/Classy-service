dfb71a412b6bfa00a6baefb1949acb0a1bd46448
Yasantha Mihiran <yasanthamax@gmail.com>
2022-09-03 18:49:25 +0530

    addBank ui created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { NotFound } from "./components/utils/NotFound/NotFound.js";
 import CreateService from "./components/CreateService";
 import AllServices from "./components/AllServices";
 import Register from "./components/Register";
+import AddBank from "./components/AddBank";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path="/" exact component={AllServices} />
         <Route path="/addService" component={CreateService} />
         <Route path="/register"  component={Register} />
+
+        <Route path="/addBank" component={AddBank} />
       </Router>
     </div>
   );
