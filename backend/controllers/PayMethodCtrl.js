06f939e55a1d034e0d6d29329af9b12a58b85849
Yasantha Mihiran <yasanthamax@gmail.com>
2022-09-04 17:02:22 +0530

    backend update bug fix

--- backend/controllers/PayMethodCtrl.js
+++ backend/controllers/PayMethodCtrl.js
@@ -13,8 +13,8 @@ const PatMethodCtrl = {
     createBank: async (req, res) => {
         try {
             const { bankName, uid, accName, accNumber, branchName } = req.body;
-            const bank = await BankData.findOne({ bankName })
-            if (bank) return res.status(400).json({ msg: "This bank already exists." })
+            const user = await BankData.findOne({ uid })
+            if (user) return res.status(400).json({ msg: "This bank already exists." })
 
             const newBank = new BankData({
                 bankName, uid, accName, accNumber, branchName
@@ -40,9 +40,9 @@ const PatMethodCtrl = {
 
     updateBank: async (req, res) => {
         try {
-            const { bankName, bankCode, accountName, accountNumber, accountType } = req.body;
+            const { bankName, uid, accName, accNumber, branchName  } = req.body;
             await BankData.findOneAndUpdate({ _id: req.params.id }, {
-                bankName, bankCode, accountName, accountNumber, accountType
+                bankName, uid, accName, accNumber, branchName 
             })
 
             res.json({ msg: "Bank updated successfully!" })
